c4abd4eadf841164597ad54c8e3c5bb9b80fd957
IrinaMarques <irinavpmarques@gmail.com>
2017-05-16 15:17:00 +0100

    2.2 Data Types
    Primitives: number, string, boolean, undefined, null
    Composite: array, object

--- js/basics.js
+++ js/basics.js
@@ -17,3 +17,31 @@ var theMeaningOfLife = 42, // numeric value
 
 // the value of foo is a string will be overwritten by an undefined value which is what variable baz contains
 foo = baz; 
+
+// DATA TYPES
+/*************
+1. Numbers
+2. String 
+   Simple
+   With Quotes
+   Escaped
+   Concatenate
+3. Array
+4. Object
+**************/
+var number = 1;
+var string = 'hello';
+var quotes = '"Hello", said the man';
+var escaped = '"Oh no you didn\'t" she said';
+var joined = 'wait, '+'what?';
+var array = [5, "something", true] 
+//length of 3 to 
+//get a value - array[0] this will get the value of 5
+var object = {
+	myNumber: 10,
+	myString: "woohoo!"
+};
+
+object.myNumber; // will return 10
+object['myString']; // woohoo!
+// The second one it's very useful when we want to use the value of a variable to choose which value we want to get from the objects
