3bb9d270d8118488e361111b6ef89166928a58c6
IrinaMarques <irinavpmarques@gmail.com>
2017-05-16 12:36:17 +0100

    2.1 Variables
    Declare variables with var.

--- js/basics.js
+++ js/basics.js
@@ -7,3 +7,13 @@ console.log('<3');
 
 // Create an alert using the alert method
 // alert('oh noes!');
+
+// THE VARIABLES
+// if we don't specified a value it's going to be undefined.
+
+var theMeaningOfLife = 42, // numeric value
+	foo = 'bar', //string
+	baz, quux; //mix declared variables with assigned variables
+
+// the value of foo is a string will be overwritten by an undefined value which is what variable baz contains
+foo = baz; 
